d50a1e15657bbf89552e52929734e608fbfa120d
chimdi-hash <unaezechimdi@gmail.com>
2026-08-13 20:46:00 +0100

    Fix __init__ must be private error

--- myogen_contract.py
+++ myogen_contract.py
@@ -28,7 +28,6 @@ class MyogenDictionary(gl.Contract):
     total_queries:   u256
     popular_terms_list: str
 
-    @gl.public.write.payable
     def __init__(self):
         self.total_queries      = u256(0)
         self.popular_terms_list = json.dumps([])
